426d7cedf0bffc72116fa16a1b710468284d0dfb
Jong Woo <vicodinc86@gmail.com>
2024-04-16 19:10:10 +0900

    add grid

--- src/components/SoundGrid.vue
+++ src/components/SoundGrid.vue
@@ -1,24 +1,43 @@
 <script setup>
+import { ref } from 'vue'
 import SoundGridItem from './SoundGridItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
+
+
+const soundGridItem = ref({
+  icon: '',
+  limitCol: 12,
+  heading: '',
+  synthTest: ''
+})
+
+//create soundGridArray
+const soundGridArray = ref([])
+
+
+//function add soundGridItem
+function addSoundGridItem() {
+  console.log('addSoundGridItem')
+  soundGridArray.value.push(soundGridItem.value)
+}
 </script>
 
 <template>
-  <!-- grid 8 x 16 -->
+  <!-- header div -->
   <div>
-    <!-- repeat inner gridItem 16 time -->
-
-    <SoundGridItem>
-      <template #icon>
-        <DocumentationIcon />
-      </template>
-      <template #heading>synthTest</template>
-
-      <!-- testbtn -->
-      <button @click="synthTest">Test Synth</button>
-    </SoundGridItem>
+    <h2>SoundGrid</h2>
   </div>
+  <!-- grid content div -->
+  <div>
 
+    <!-- addGridButton -->
+    <button @click="addSoundGridItem">Add Grid Item</button>
 
-
+    <!-- grid-->
+    <div class="grid grid-cols-12 gap-4">
+      <template v-for="(item, index) in soundGridArray" :key="index">
+        <SoundGridItem :item="item">
+        </SoundGridItem>
+      </template>
+    </div>
+  </div>
 </template>
